717d5fe4da1c5a42ae5c0ea32df1109fd158d678
marcusleem <Thengtheng5>
2020-05-04 10:08:51 +0800

    Commit

---
+++ app/src/main/java/com/marcus/demoimageview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.marcus.demoimageview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView ivDay1;
+    ImageView ivDay2;
+    ImageView ivDay3;
+    ImageView ivDay4;
+    ImageView ivDay5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ivDay1 = (ImageView) findViewById(R.id.imageView);
+        ivDay2 = (ImageView) findViewById(R.id.imageView2);
+        ivDay3 = (ImageView) findViewById(R.id.imageView3);
+        ivDay4 = (ImageView) findViewById(R.id.imageView4);
+        ivDay5 = (ImageView) findViewById(R.id.imageView5);
+
+        ivDay1.setImageResource(R.drawable.day1);
+        ivDay2.setImageResource(R.drawable.day2);
+        ivDay3.setImageResource(R.drawable.day3);
+        ivDay4.setImageResource(R.drawable.day4);
+        ivDay5.setImageResource(R.drawable.day5);
+
+
+    }
+}
